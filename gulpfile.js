a5c01b754c9b9fbaaae62341a4f0bfc44fd99da8
Jason Hemmy <jasonhemmy@gmail.com>
2023-06-13 16:06:03 -0500

    get gulp uglify running

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
+'use strict';
 
-exports.default = defaultTask
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var sass = require('gulp-sass')(require('sass'));
+
+gulp.task('default', function () {
+    return gulp.src('chill-utils.js')
+        .pipe(uglify())
+        .pipe(rename('chill-utils.min.js'))
+        .pipe(gulp.dest('./dist/'))
+});
