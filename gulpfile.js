d99a99b1e4658a3fcaf35b8d2acf52427c0c296c
Jason Hemmy <jasonhemmy@gmail.com>
2023-06-13 16:24:52 -0500

    move to src folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var rename = require("gulp-rename");
 var sass = require('gulp-sass')(require('sass'));
 
 gulp.task('default', function () {
-    return gulp.src('chill-utils.js')
+    return gulp.src('./src/chill-utils.js')
         .pipe(uglify())
         .pipe(rename('chill-utils.min.js'))
         .pipe(gulp.dest('./dist/'))
