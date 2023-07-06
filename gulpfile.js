9e094e9c6ea174cac9f1f0ff5273a794a2fd8859
Jason Hemmy <jasonhemmy@gmail.com>
2023-07-05 19:38:18 -0500

    finish up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 'use strict';
 
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require("gulp-rename");
-var sass = require('gulp-sass')(require('sass'));
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require("gulp-rename");
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('default', function () {
+gulp.task('scripts', () => {
     return gulp.src('./src/chill-utils.js')
         .pipe(uglify())
         .pipe(rename('chill-utils.min.js'))
         .pipe(gulp.dest('./dist/'))
 });
+
+gulp.task('styles', () => {
+    return gulp.src('./src/chill-utils.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename('chill-utils.min.css'))
+        .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('default', gulp.series(['scripts', 'styles']));
